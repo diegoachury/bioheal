89a86558cd975b0badfe7ec9ad1db8c8649c025a
diegoachury <tiran_o@hotmail.com>
2014-01-20 00:36:59 +0000

    creando repositorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bioheal::Application.config.secret_key_base = 'eb3b8d82f5b47ae76bcb8efb34bf7448cd5718c53fe6c03a9ec7deb15f68da3713de5c13f875d017c49e161f9241ebe4c4db243fba8d17e08839c3b259b44a31'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bioheal::Application.config.session_store :cookie_store, key: '_bioheal_session'
